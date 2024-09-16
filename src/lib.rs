72c42d19a157e9bde3efd65f052c98cb2f85ef48
周志鹏 <823143047@qq.com>
2024-09-16 12:02:43 +0400

    add makefile

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
-#[cfg(feature = "common_crypt")]
+#[cfg(feature = "common-crypt")]
 pub use common_crypt::*;
 #[cfg(feature = "blockchain")]
 pub use blockchain::*;
 #[cfg(feature = "data-api")]
 pub use data_api::*;
+#[cfg(feature = "sql-util")]
+pub use sql_util::*;
